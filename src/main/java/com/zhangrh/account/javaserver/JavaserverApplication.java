f45f6acaf21b8c40d076e3799369b1032174e463
zhangrh <runhaozhang@sohu-inc.com>
2021-02-04 18:14:44 +0800

    init

---
+++ src/main/java/com/zhangrh/account/javaserver/JavaserverApplication.java
@@ -0,0 +1,13 @@
+package com.zhangrh.account.javaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zhangrh/account/javaserver/JavaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangrh.account.javaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
