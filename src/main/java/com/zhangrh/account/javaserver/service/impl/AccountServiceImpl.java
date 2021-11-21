392b2c5057301c9ddfc268cc5826c6d9b35fc28f
zhangrh <runhaozhang@sohu-inc.com>
2021-11-21 10:41:47 +0800

    feat: caculate banlance

--- src/main/java/com/zhangrh/account/javaserver/service/impl/AccountServiceImpl.java
+++ src/main/java/com/zhangrh/account/javaserver/service/impl/AccountServiceImpl.java
@@ -9,7 +9,9 @@ import com.zhangrh.account.javaserver.entity.Account;
 import com.zhangrh.account.javaserver.exception.Asserts;
 import com.zhangrh.account.javaserver.mapper.AccountMapper;
 import com.zhangrh.account.javaserver.service.AccountService;
+import com.zhangrh.account.javaserver.service.TradeService;
 import com.zhangrh.account.javaserver.service.Bo.AccountBo;
+import com.zhangrh.account.javaserver.service.Bo.TradeBo;
 import com.zhangrh.account.javaserver.service.Bo.UserBo;
 
 import org.slf4j.Logger;
@@ -24,6 +26,9 @@ public class AccountServiceImpl implements AccountService {
 
   @Autowired
   AccountMapper accountMapper;
+
+  @Autowired
+  TradeService tradeService;
 
   @Override
   @Transactional
@@ -45,8 +50,9 @@ public class AccountServiceImpl implements AccountService {
       List<Account> accounts = accountMapper.queryByUser(userBo.toUser());
       for (Account account : accounts) {
         if (account.getDeleteAt() == null) { // 排除已经删除的账户
-          // TODO:  此处统计交集记录, 并计算金额
-          accountBos.add(new AccountBo(account));
+          AccountBo accountBo = new AccountBo(account);
+          accountBo.setMoney(calculateBalance(accountBo));
+          accountBos.add(accountBo);
         }
       }
     } catch (Exception e) {
@@ -61,12 +67,15 @@ public class AccountServiceImpl implements AccountService {
     Account account = null;
     try {
       account = accountMapper.queryId(accountBo.getId());
-      if (account.getDeleteAt() != null) throw new Error("account is deleted");
+      accountBo = new AccountBo(account);
+      accountBo.setMoney(calculateBalance(accountBo));
+      if (account.getDeleteAt() != null)
+        throw new Error("account is deleted");
     } catch (Exception e) {
       LOGGER.warn(e.getMessage());
       Asserts.fail("查询账户信息失败");
     }
-    return new AccountBo(account);
+    return accountBo;
   }
 
   @Override
@@ -91,10 +100,21 @@ public class AccountServiceImpl implements AccountService {
     }
   }
 
-
   @Override
   public BigDecimal calculateBalance(AccountBo accountBo) {
-    // TODO Auto-generated method stub
-    return null;
+    List<TradeBo> tradeBos = tradeService.list(accountBo);
+    BigDecimal result = new BigDecimal(0);
+    for (TradeBo tradeBo : tradeBos) {
+      BigDecimal money = tradeBo.getMoney();
+      switch (tradeBo.getOperate().getSign()) {
+      case "subtract":
+        result = result.subtract(money);
+        break;
+      case "add":
+        result = result.add(money);
+        break;
+      }
+    }
+    return result;
   }
 }
